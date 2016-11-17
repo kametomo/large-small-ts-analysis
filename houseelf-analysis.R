af2a83f190b1da975f6e5c1c1a4f7fe8579edbcd
Hirama <Tomo.Hirama@MyFWC.com>
2016-11-17 13:25:25 -0500

    Imported the houseelf data from the data folder

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -2,4 +2,8 @@
 #2. -- First Commit -- ####
 #This is the investigation of earlength and DNA relationship on house elf. 
 
-#3. -- Importing Data --
+#3. -- Importing Data --####
+#3.1 
+#3.2
+#3.3
+houseelf_dat <- read.csv("data/houseelf-earlength-dna-data.csv")
